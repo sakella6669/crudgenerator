7f6781df6399c27dcdbc76af7422360959256ae1
liutong628@gmail.com <liutong628@gmail.com@85c3109b-16f1-73ce-5089-98dc613a25be>
2011-12-11 15:56:42 +0000

    Simplify the model defining.<br/> 1.use the xml schema default value.<br/> 2.use the default value setter to process.

--- trunk/crudgenerator/src/main/java/com/bupt/liutong/Test.java
+++ trunk/crudgenerator/src/main/java/com/bupt/liutong/Test.java
@@ -11,10 +11,9 @@ import javax.xml.parsers.SAXParserFactory;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
+import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
 
 public class Test {
 
@@ -22,26 +21,45 @@ public class Test {
 
 	public static void main(String[] args) throws SAXException,
 			ParserConfigurationException, IOException, DocumentException {
-		SAXParserFactory factory = SAXParserFactory.newInstance();
-		factory.setValidating(true);
-
-		SAXParser parser = factory.newSAXParser();
-		parser.setProperty("http://java.sun.com/xml/jaxp/properties/schemaLanguage", 
-		      "http://www.w3.org/2001/XMLSchema");
-
-		SAXReader reader = new SAXReader(parser.getXMLReader());
-		Map<String, String> map = new HashMap<String, String>();
-	    map.put("config","http://www.w3school.com.cn");
-		reader.getDocumentFactory().setXPathNamespaceURIs(map);
-		// reader.setErrorHandler(new SimpleErrorHandler());
-		Document config = reader.read(BASE_PATH
-				+ "/src/main/java/com/bupt/liutong/generator2/config/".replace(
-						"/", File.separator) + "config.xml");
+		SAXParserFactory configFactory = SAXParserFactory.newInstance();
+		configFactory.setValidating(true);
+		SAXParser configParser = configFactory.newSAXParser();
+		configParser.setProperty("http://java.sun.com/xml/jaxp/properties/schemaLanguage", 
+			      "http://www.w3.org/2001/XMLSchema");
+		SAXReader configReader = new SAXReader(configParser.getXMLReader());
+		Map<String, String> configSchema = new HashMap<String, String>();
+		configSchema.put("yaoming","http://www.w3school.com.cn");
+		configReader.getDocumentFactory().setXPathNamespaceURIs(configSchema);
+		Document config = configReader.read(BASE_PATH
+				+ "/src/main/java/com/bupt/liutong/generator2/config/".replace("/",
+						File.separator) + "config.xml");
+		/**
+		 * 
+		 */
+		SAXParserFactory modelFactory = SAXParserFactory.newInstance();
+		modelFactory.setValidating(true);
+		SAXParser modelParser = modelFactory.newSAXParser();
+		modelParser.setProperty("http://java.sun.com/xml/jaxp/properties/schemaLanguage", 
+			      "http://www.w3.org/2001/XMLSchema");
+		SAXReader modelReader = new SAXReader(modelParser.getXMLReader());
+		Map<String, String> modelSchema = new HashMap<String, String>();
+		modelSchema.put("m","http://www.w3school.com.cn");
+		modelReader.getDocumentFactory().setXPathNamespaceURIs(modelSchema);
+		Document model = modelReader.read(BASE_PATH
+				+ "/src/main/java/com/bupt/liutong/generator2/config/".replace("/",
+						File.separator) + "model.xml");
+		Element modelRoot = model.getRootElement();
+		/**
+		 * 
+		 */
+		
+		
 //		System.out.println(config.getRootElement().selectSingleNode(
 //				"/*[local-name()='config' and namespace-uri()='http://www.w3school.com.cn']/*[local-name()='db' and namespace-uri()='http://www.w3school.com.cn']"));
 		String targetPrj = config.getRootElement().selectSingleNode(
-				"//config:config/config:parameters/config:parameter[@id='targetPrj']").valueOf(
-				"@value");
+				"//config:config/config:templates/config:template[@path='src/main/java/com/bupt/liutong/generator2/template/dto.java.xsl']").valueOf(
+				"@override");
+		System.out.println(targetPrj);
 	}
 
 }
---
+++ trunk/crudgenerator/src/main/java/com/bupt/liutong/generator2/setter/ColumnNameSetter.java
@@ -0,0 +1,34 @@
+package com.bupt.liutong.generator2.setter;
+
+import java.util.List;
+
+import org.dom4j.Element;
+
+import com.bupt.liutong.util.StringUtils;
+
+public class ColumnNameSetter implements DefaultValueSetter {
+
+	public Element setDefaultValue(Element rootElement) {
+		@SuppressWarnings("unchecked")
+		List<Element> packages = rootElement.elements();
+		for(Element pkg : packages){
+			@SuppressWarnings("unchecked")
+			List<Element> tables = pkg.elements();
+			for (Element table : tables) {
+				@SuppressWarnings("unchecked")
+				List<Element> columns = table.elements();
+				for (Element column : columns) {
+					if (column.getName().equals("column")) {
+						String field = column.attributeValue("field");
+						if (StringUtils.isEmpty(field))
+							throw new NullPointerException("column field should not be null");
+						if (StringUtils.isEmpty(column.attributeValue("name")))
+							column.addAttribute("name", field);
+					}
+				}
+			}
+		}
+		return rootElement;
+	}
+	
+}
